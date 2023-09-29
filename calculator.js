c46ce146636de3062a5332eb3944c0f231882b4f
popo26 <mynameispopo26@gmail.com>
2023-09-29 18:19:29 +1300

    Reset the screen

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 let add = document.querySelector(".add");
 let subtract = document.querySelector(".subtract");
 let result = document.getElementById("result");
+let clear = document.querySelector(".clear");
 
 const addition = () => {
   let num1 = document.getElementById("num1").value;
@@ -24,4 +25,11 @@ subtract.addEventListener("click", function () {
   document.getElementById("operator").innerHTML = "-";
   let total = parseFloat(subtraction());
   result.innerHTML = total;
+});
+
+clear.addEventListener("click", function () {
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
+  document.getElementById("operator").innerHTML = "";
+  result.innerHTML = "";
 });
