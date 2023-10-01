6b5cc3722055c0567f84b893d86f28724165e9a9
popo26 <mynameispopo26@gmail.com>
2023-10-02 08:45:47 +1300

    changed from e.target.value to just .value

--- calculator.js
+++ calculator.js
@@ -2,14 +2,13 @@ let num1 = document.getElementById("num1");
 let num2 = document.getElementById("num2");
 
 num1.addEventListener("input", function (e) {
-  console.log(e.target.value);
-  console.log(typeof e.target.valueAsNumber);
-  // return num1.valueAsNumber;
+  // console.log(e.target.value);
+  // console.log(typeof e.target.valueAsNumber);
+  console.log(num1.value);
 });
 
 num2.addEventListener("input", function (e) {
-  console.log(e.target.value);
-  console.log(typeof e.target.valueAsNumber);
-
-  // return num2.valueAsNumber;
+  // console.log(e.target.value);
+  // console.log(typeof e.target.valueAsNumber);
+  console.log(num2.value);
 });
