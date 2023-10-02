105ae4546a02f7c2dee9dead21e773d1cce63459
popo26 <mynameispopo26@gmail.com>
2023-10-02 13:42:38 +1300

    reviewed functionality

--- calculator.js
+++ calculator.js
@@ -1,27 +1,51 @@
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
 let add = document.querySelector(".add");
 let subtract = document.querySelector(".subtract");
 let result = document.getElementById("result");
+let equal = document.querySelector(".equal");
 
 const addition = () => {
-  let num1 = document.getElementById("num1").value;
-  let num2 = document.getElementById("num2").value;
+  let num1 = parseFloat(document.getElementById("num1").value);
+  let num2 = parseFloat(document.getElementById("num2").value);
   return num1 + num2;
 };
 
 const subtraction = () => {
-  let num1 = document.getElementById("num1").value;
-  let num2 = document.getElementById("num2").value;
+  let num1 = parseFloat(document.getElementById("num1").value);
+  let num2 = parseFloat(document.getElementById("num2").value);
   return num1 - num2;
 };
 
+// Debug Purpose
+// num1.addEventListener("input", function () {
+//   console.log(num1.value);
+// });
+
+// num2.addEventListener("input", function () {
+//   console.log(num2.value);
+// });
+
 add.addEventListener("click", function () {
-  document.getElementById("operator").innerHTML = "+";
-  let total = parseFloat(addition());
-  result.innerHTML = total;
+  operator.innerHTML = "+";
 });
 
 subtract.addEventListener("click", function () {
-  document.getElementById("operator").innerHTML = "-";
-  let total = parseFloat(subtraction());
-  result.innerHTML = total;
+  operator.innerHTML = "-";
+});
+
+equal.addEventListener("click", function () {
+  if (num1.value === "" || num2.value === "") {
+    result.innerHTML = "Please enter a number in both fields.";
+  } else if (operator.innerHTML === "") {
+    result.innerHTML = "Please select an operator.";
+  } else {
+    if (operator.innerHTML === "+") {
+      let total = addition();
+      result.innerHTML = total;
+    } else {
+      let total = subtraction();
+      result.innerHTML = total;
+    }
+  }
 });
